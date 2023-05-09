d4d849cbc6b9497f491f1ed4134f6739117fbce2
serhii.zh <serhii.zh@gmail.com>
2023-05-09 17:29:46 +0300

    item changes

--- src/components/HomePage/Main/ItemsBlock/Item/Item.jsx
+++ src/components/HomePage/Main/ItemsBlock/Item/Item.jsx
@@ -1,4 +1,5 @@
 import styles from './Item.module.css';
+import heartInactive from '../../../../../images/heart_inactive.png';
 
 const Item = ({ imageUrl, title, price }) => {
   if (title.length > 20) {
@@ -7,6 +8,9 @@ const Item = ({ imageUrl, title, price }) => {
   return (
     <div className={styles.item}>
       <img src={imageUrl} alt='product' />
+      <div className={styles.addToFavorites}>
+        <img src={heartInactive} alt='add-to-favorites-i' />
+      </div>
       <p>{title}</p>
       <span>${price}</span>
     </div>
