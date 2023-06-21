1d4c68aa4089b2acb1176b6ecf96614771b6d0f9
serhii.zh <serhii.zh@gmail.com>
2023-06-21 13:51:16 +0300

    added a fix to how the product name is displayed

--- src/components/HomePage/Main/Products/ItemsBlock/Item/Item.jsx
+++ src/components/HomePage/Main/Products/ItemsBlock/Item/Item.jsx
@@ -2,9 +2,6 @@ import styles from './Item.module.css';
 import heartInactive from '../../../../../../images/heart_inactive.png';
 
 const Item = ({ imageUrl, title, price }) => {
-  if (title.length > 20) {
-    title = title.slice(0, 20) + '...';
-  }
   return (
     <div className={styles.item}>
       <img src={imageUrl} alt='product' />
