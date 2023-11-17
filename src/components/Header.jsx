6a1e755ebd34db108aa678752dc66f841bebc971
serhii.zh <serhii.zh@gmail.com>
2023-11-17 16:55:19 +0200

    refactor: changed some components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,69 @@
-import { FavoritesIcon } from './ui/FavoritesIcon';
-import { ShoppingCart } from './ui/ShoppingCart';
 import styles from '../styles/components/Header.module.css';
-import { Logo } from './ui/Logo';
-import { currentUser } from '../store/slices/currentUserSlice';
-import { RegisterLogin } from './ui/RegisterLogin';
-import { UserBlock } from './ui/UserBlock';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { userToken } from '../services/axiosInstances';
+import { useNavigate } from 'react-router-dom';
+
+import { currentUser } from '../store/slices/currentUserSlice';
+import { clearData } from '../store/slices/productsSlice';
 import { getAccountData } from '../store/thunks/userThunks';
+
+import { userToken } from '../services/axiosInstances';
+
+import logoImg from '../images/logo.png';
+import hFavoritesImg from '../images/heart_white.png';
+import hCartImg from '../images/cart_white.png';
+
+import { StyledLogo } from './ui/StyledLogo';
+import { StyledHeaderIcon } from './ui/StyledHeaderIcon';
+
+import { RegisterLogin } from './ui/RegisterLogin';
+import { UserBlock } from './ui/UserBlock';
 
 export const Header = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const userData = useSelector(currentUser);
 
   useEffect(() => {
     userToken && dispatch(getAccountData());
   }, [dispatch]);
 
+  const handleLogoClick = () => {
+    dispatch(clearData());
+    navigate('/');
+  };
+
+  const handleFavoritesClick = () => {
+    navigate('/settings/favorites');
+  };
+
+  const handleShoppingCartClick = () => {
+    navigate('/shopping-cart');
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.logoTitleBlock}>
-        <Logo />
+        <StyledLogo
+          src={logoImg}
+          alt='Everything U Need'
+          onClick={handleLogoClick}
+        />
         <h2>Everything U Need</h2>
       </div>
       <div className={styles.shoppingLoggingBlock}>
         <div className={styles.favoritesCartBlock}>
-          <FavoritesIcon />
-          <ShoppingCart />
+          <StyledHeaderIcon
+            src={hFavoritesImg}
+            alt='Favorites'
+            onClick={handleFavoritesClick}
+          />
+          <StyledHeaderIcon
+            src={hCartImg}
+            alt='Shopping Cart'
+            onClick={handleShoppingCartClick}
+          />
         </div>
         {userData ? <UserBlock /> : <RegisterLogin />}
       </div>
